03cbe6a78919ccafd8e1312696e406d1eab5f5d4
Luis Pena <luismiguelpvalero@gmail.com>
2023-01-13 23:03:29 -0300

    versionfinalreact

--- entrega01/src/components/Checkout.jsx
+++ entrega01/src/components/Checkout.jsx
@@ -1,30 +1,31 @@
-import { addDoc, collection, getFirestore } from "firebase/firestore";
-import React, { useContext } from "react";
-import { useState } from "react";
+import React, { useContext, useState } from "react";
+import { addDoc, doc, collection, getFirestore, updateDoc} from "firebase/firestore";
 import { CartContext } from "./context/CartContext";
 
 const Checkout = () => {
-    const {cart, sumTotal} = useContext(CartContext)
+    const {cart, clear, sumTotal} = useContext(CartContext);
     const [nombre, setNombre] = useState("");
     const [telefono, setTelefono] = useState("");
     const [correo, setCorreo] = useState("");
-    const [orderId, setOrderId] = useState("");   
-    
-    console.log(orderId);
+    const [orderId, setOrderId] = useState("");
 
     const generarOrden = () => {
         const fecha = new Date();
-        const orders = {
-            buyer:{name:nombre, telephone:telefono, email:correo},
-            items: cart.map(item =>({id:item.id, tittle:item.nombre, price:item.precio})),
-            total:sumTotal(),
-            order_date: `${fecha.getFullYear()}-${fecha.getMonth() + 1}-${fecha.getDate()} ${fecha.getHours()}:${fecha.getMinutes()}:${fecha.getSeconds()}`
+        const order = {
+            buyer: {name:nombre, telephone:telefono, email: correo},
+            items: cart.map(item => ({id:item.id, title:item.nombre, quantity:item.quantity, price:item.precio, price_total:item.quantity * item.precio})),
+            total: sumTotal(),
+            order_date: `${fecha.getFullYear()}/${fecha.getMonth() + 1}/${fecha.getDate()} ${fecha.getHours()}:${fecha.getMinutes()}:${fecha.getSeconds()}`
         };
 
         const db = getFirestore();
         const ordersCollection = collection(db, "orders");
-        addDoc(ordersCollection, orders).then((snapShot) => {
+        addDoc(ordersCollection, order).then((snapShot) => {
             setOrderId(snapShot.id);
+            const generatedOrder = doc(db, "orders", snapShot.id); 
+            updateDoc(generatedOrder, {total:order.total * 1.21}); 
+
+            clear();
         });
     }
 
@@ -34,44 +35,44 @@ const Checkout = () => {
                 <div className="col-md-6">
                     <form>
                         <div className="mb-3">
-                            <label htmlFor="nombre" className="form-label">Nombre</label>
-                            <input type="text" className="form-control" id="nombre" placeholder="Ingresa tu nombre" onInput={(e) => {setNombre(e.target.value)}}></input>
+                            <label htmlFor="nombre" className="form-label">Nombre:</label>
+                            <input type="text" className="form-control" placeholder="Ingrese su Nombre" onInput={(e) => {setNombre(e.target.value)}} />
                         </div>
                         <div className="mb-3">
-                            <label htmlFor="telefono" className="form-label">Teléfono</label>
-                            <input type="number" className="form-control" id="telefono" placeholder="Ingresa tu número de contacto" onInput={(e) => {setTelefono(e.target.value)}}></input>
+                            <label htmlFor="telefono" className="form-label">Teléfono:</label>
+                            <input type="number" className="form-control" id="telefono" placeholder="Ingrese su Teléfono" onInput={(e) => {setTelefono(e.target.value)}} />
                         </div>
                         <div className="mb-3">
-                            <label htmlFor="email" className="form-label">Correo</label>
-                            <input type="email" className="form-control" id="nombre" placeholder="Ingresa tu correo" onInput={(e) => {setCorreo(e.target.value)}}></input>
+                            <label htmlFor="correo" className="form-label">Correo</label>
+                            <input type="text" className="form-control" id="correo" placeholder="Ingrese su correo" onInput={(e) => {setCorreo(e.target.value)}} />
                         </div>
-
-                        <button type="submit" className="btn btn-warning" onClick={generarOrden}>Enviar</button>
+                        <button type="button" className="btn btn-warning" onClick={generarOrden}>Generar Orden</button>
                     </form>
                 </div>
                 <div className="col-md-6">
-                    <table className="table">
+                    <table className="table">                                
                         <tbody>
                             {cart.map(item => (
                                 <tr key={item.id}>
                                     <td><img src={item.imagen} alt={item.nombre} width={80} /></td>
                                     <td className="align-middle">{item.nombre}</td>
-                                    <td className="align-middle">{item.quantity}</td>
-                                    <td className="align-middle">${item.quantity * item.precio}</td>                              
+                                    <td className="align-middle text-end">{item.quantity}</td>
+                                    <td className="align-middle text-end">${item.quantity * item.precio}</td>
                                 </tr>
-                            ))}
+                                ))
+                            }
                             <tr>
-                                <td colSpan={2}></td>
-                                <td className="align-middle">Total a Pagar</td>
-                                <td className="align-middle">${sumTotal()}</td>
+                                <td colSpan={2}>&nbsp;</td>
+                                <td className="text-end"><b>Total a Pagar</b></td>
+                                <td className="text-end"><b>${sumTotal()}</b></td>
                             </tr>
                         </tbody>
-                    </table>
+                    </table>    
                 </div>
             </div>
             <div className="row">
                 <div className="col text-center">
-                    {orderId !== "" ? <div className="alert alert-warning" role="alert"> La orden es: {orderId}</div> : "" }
+                    {orderId !== "" ? <div className="alert alert-warning" role="alert">Su orden es: <b>{orderId}</b></div> : ""}
                 </div>
             </div>
         </div>
